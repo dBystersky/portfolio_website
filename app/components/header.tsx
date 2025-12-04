413e701908f8e98561d6de4980df91b68bdce7cf
dbys0001 <dbys0001@student.monash.edu>
2025-12-04 19:44:28 +1100

    Some small stylistic changes

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,22 +1,25 @@
-import React from "react";
 import Link from "next/link";
 
 export default function WebsiteHeader() {
     return (
-        <header className="w-full px-8 py-4 bg-white shadow-md sticky top-0 z-50 ">
+        <header className="w-full bg-white shadow-md sticky top-0 z-50 ">
+            <div className="max-w-6xl mx-auto flex items-center justify-between px-6 py-4">
+                {/* Left / Logo */}
+                <div className="flex items-center gap-3">
+                    <h1 className="text-2xl font-bold text-gray-900 tracking-tight hover:text-gray-600 transition">
+                        <Link href="/">Darcy Bystersky</Link>
+                    </h1>
+                </div>
+                
+                { /* Nav */}
+                <nav className="hidden md:flex gap-6 justify-between items-center text-black font-medium"> 
+                    <Link href="/" className="hover:text-gray-600 hover:outline transition">Home</Link>
+                    <Link href="/about" className="hover:text-gray-600 hover:outline transition">About Me</Link>
+                    <Link href="/portfolio" className="hover:text-gray-600 hover:outline transition">Portfolio</Link>
+                    <Link href="/contact" className="hover:text-gray-600 hover:outline transition">Contact</Link>
+                </nav>
 
-            { /* Left */}
-            <div className="flex items-center gap-3">
-                <h1 className="text-xl font-bold semi-bold text-black">Darcy Bystersky</h1>
             </div>
-
-            { /* Centre */}
-            <nav className="hidden md:flex gap-6 justify-between items-center text-black font-medium">
-                <Link href="/" className="hover:text-gray-600 hover:outline transition">Home</Link>
-                <Link href="/about" className="hover:text-gray-600 hover:outline transition">About Me</Link>
-                <Link href="/portfolio" className="hover:text-gray-600 hover:outline transition">Portfolio</Link>
-                <Link href="/contact" className="hover:text-gray-600 hover:outline transition">Contact</Link>
-            </nav>
 
         </header>
     )

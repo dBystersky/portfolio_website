91c69eb3e247e35c95fdfea7c25c9f89ad96162b
dbys0001 <dbys0001@student.monash.edu>
2025-12-04 21:06:25 +1100

    Added footer to other pages

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,5 @@
 import WebsiteHeader from "../components/header";
-
+import WebsiteFooter from "../components/footer";
 
 export default function AboutPage() {
   return (   
@@ -10,7 +10,8 @@ export default function AboutPage() {
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
           <h1>This is the about me page</h1>
       </main>
-  
+      
+      <WebsiteFooter />
     </>
   );
 }
--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,4 +1,5 @@
 import WebsiteHeader from "../components/header";
+import WebsiteFooter from "../components/footer";
 
 
 export default function PortfolioPage() {
@@ -10,7 +11,8 @@ export default function PortfolioPage() {
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
           <h1>This is the portfolio page</h1>
       </main>
-  
+
+      <WebsiteFooter />
     </>
   );
 }
